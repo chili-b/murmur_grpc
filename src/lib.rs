9b144d5bb37a1421c352784270b4331f72353ffa
chili-b <dexter.gaonshatford@gmail.com>
2020-12-04 13:51:14 -0500

    hopefully resolved the locking issue with the filter stream

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,8 @@ use futures::Future;
 use tonic::transport::Endpoint;
 use tonic::codegen::StdError;
 
-use tokio::sync::{Mutex, MutexGuard, mpsc::{self, Sender, Receiver}};
+use tokio::sync::{Mutex, MutexGuard, mpsc};
 use tokio::runtime::Builder;
-use tokio::stream::Stream;
 
 use std::convert::TryInto;
 use std::sync::Arc;
@@ -95,6 +94,7 @@ impl<T> DataMutex<T>
             t: Arc::new(Mutex::new(t))
         }
     }
+
     /// Lock the Mutex synchronously while inside a tokio runtime. Calling this method while
     /// outside of a tokio runtime will cause a panic.
     pub fn lock(&mut self) -> MutexGuard<T> {
@@ -193,109 +193,107 @@ where A: TryInto<Endpoint> + Send + Clone,
       A::Error: Into<StdError>,
       T: Send + Clone
 {
+    /// Create a new MurmurInterfaceBuilder provided a data value and an addr to which the gRPC
+    /// connection will be made
     pub fn new(t: T, addr: A) -> Self {
         Self {
             i: MurmurInterface::new(t, addr)
         }
     }
+    /// Assign the the value that will be used to represent state
     pub fn data(mut self, t: T) -> Self {
         self.i.t = DataMutex{t: Arc::new(Mutex::new(t))};
         self
     }
+    /// Alternative method to provide the value for state if it is already wrapped in a
+    /// [DataMutex](struct.DataMutex.html)
     pub fn data_mutex(mut self, t: DataMutex<T>) -> Self {
         self.i.t = t;
         self
     }
+    /// Set the address to which the connection will be made
     pub fn addr(mut self, addr: A) -> Self {
         self.i.addr = addr;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will respond to users connecting to the
+    /// server
     pub fn user_connected(mut self, user_connected: Vec<Handler<T>>) -> Self {
         self.i.user_connected = user_connected;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will respond to users disconnecting from the
+    /// server
     pub fn user_disconnected(mut self, user_disconnected: Vec<Handler<T>>) -> Self {
         self.i.user_disconnected = user_disconnected;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will respond to users' states changing (mute,
+    /// unmute, change avatar, etc.)
     pub fn user_state_changed(mut self, user_state_changed: Vec<Handler<T>>) -> Self {
         self.i.user_state_changed = user_state_changed;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will respond to users sending text messages
     pub fn user_text_message(mut self, user_text_message: Vec<Handler<T>>) -> Self {
         self.i.user_text_message = user_text_message;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will repsond to channels being created.
     pub fn channel_created(mut self, channel_created: Vec<Handler<T>>) -> Self {
         self.i.channel_created = channel_created;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will respond to channels being removed.
     pub fn channel_removed(mut self, channel_removed: Vec<Handler<T>>) -> Self {
         self.i.channel_removed = channel_removed;
         self
     }
+    /// Provide the [Handler](type.Handler.html)s that will respond to channels' states changing
+    /// (renamed, linked, description updated, etc.)
     pub fn channel_state_changed(mut self, channel_state_changed: Vec<Handler<T>>) -> Self {
         self.i.channel_state_changed = channel_state_changed;
         self
     }
+    /// Provide the [ChatFilter](type.ChatFilter.html)s that will filter text messages sent by
+    /// users.
     pub fn chat_filters(mut self, chat_filters: Vec<ChatFilter<T>>) -> Self {
         self.i.chat_filters = chat_filters;
         self
     }
+    /// Provide the [Authenticator](type.Authenticator.html)s that will handle user authentication
+    /// events (register, unregister, authenticate, etc.)
     pub fn authenticators(mut self, authenticators: Vec<Authenticator<T>>) -> Self {
         self.i.authenticators = authenticators;
         self
     }
+    /// This one is less straightforward. I'm not explaining it right now because I'm lazy.
     pub fn context_actions(mut self, context_actions: Vec<(ContextAction, Vec<ContextActionHandler<T>>)>) -> Self {
         self.i.context_actions = context_actions;
         self
     }
+    /// Provide the [ConnectHandler](type.ConnectHandler.html)s that will respond to a connection
+    /// to the Mumble server being established.
     pub fn server_connected(mut self, connection_handlers: Vec<ConnectHandler<T>>) -> Self {
         self.i.server_connected = connection_handlers;
         self
     }
+    /// Provide the [DisconnectHandler](type.DisconnectHandler.html)s that will respond to the
+    /// connection to the Mumble server being closed.
     pub fn server_disconnected(mut self, disconnection_handlers: Vec<DisconnectHandler<T>>) -> Self {
         self.i.server_disconnected = disconnection_handlers;
         self
     }
+    /// Return a clone of the contained MurmurInterface
     pub fn build(&self) -> MurmurInterface<A, T> {
         self.i.clone()
     }
 }
 
-/// Start a connection to a Mumble server for each provided MurmurInterface
-/// 
-/// # Arguments
-///
-/// * `num_threads` - The number of threads available to the threadpool on which the connections
-/// will run.
-///
-/// * `interfaces` - The data required to set up each connection
-///
-/// # Return value
-///
-/// Returns a Vec of tuples. The first tuple element is the Client needed to communicate with each
-/// server. The second tuple element is a DataMutex that contains the persistent data for each
-/// server. The third tuple element is a JoinHandle<()> that can be used to detect when the
-/// connection to the server closes. The MurmurInterface passed can alternatively be configured to
-/// auto-reconnect, so if that is the beahvior you desire you don't need to worry about the
-/// JoinHandle
-///
-pub fn start<A, T>(interfaces: Vec<MurmurInterface<A, T>>) -> Vec<(Client, DataMutex<T>, JoinHandle<()>)>
-where A: TryInto<Endpoint> + Send + 'static + Clone,
-      A::Error: Into<StdError>,
-      T: Send + Clone + 'static,
-{
-    let mut result_vec = vec![];
-    for i in interfaces.into_iter() {
-        // for whatever reason, we cannot pass this client into each child thread as trying to use
-        // its streams will panic if we do.
-        let c = runtime(V1Client::connect(i.addr.clone())).unwrap();
-        result_vec.push((c, i.t.clone(), start_connection(i)));
-    }
-    result_vec
-}
 
+/// Start a connection to a Mumble server using the given [MurmurInterface](struct.MurmurInterface.html).
+/// The function's body spawns a new thread and returns its join handle. I reccommend using
+/// [MurmurInterfaceBuilder](struct.MurmurInterfaceBuilder.html) to generate MurmurInterfaces.
 pub fn start_connection<A, T>(i: MurmurInterface<A, T>) -> JoinHandle<()>
 where T: Send + Clone + 'static,
       A: TryInto<Endpoint> + Send + 'static + Clone,
@@ -308,7 +306,8 @@ where T: Send + Clone + 'static,
                 start_single(i_clone).await;
                 // Don't iterate more than once if this interface is not configure to
                 // auto-reconnect.
-                if !i.auto_reconnect {break;}
+                if !i.auto_reconnect { break; }
+
                 thread::sleep(time::Duration::from_secs(RECONNECT_DELAY_SECONDS));
             }
         });
@@ -353,12 +352,13 @@ where T: Send + Clone + 'static,
         running: Some(true),
         uptime: None,
     };
+
     // SERVER EVENTS
     let server_event_fut = {
         let mut c = c.clone();
         let t = t.clone();
         let server = server.clone();
-        future_from_async(async move {
+        Box::pin(async move {
             if !user_connected.is_empty() || !user_disconnected.is_empty() || !user_state_changed.is_empty() 
                 || !user_text_message.is_empty() || !channel_created.is_empty() || !channel_removed.is_empty() 
                     || !channel_state_changed.is_empty()
@@ -366,22 +366,24 @@ where T: Send + Clone + 'static,
                 let mut event_stream = c.server_events(server).await
                     .expect("Connecting to the event stream")
                     .into_inner();
-                while let Ok(Some(event)) = event_stream.message().await {
-                    // the generated method name 'type' conflics with a rust keyword so 'r#' is needed
-                    match event.r#type() {
-                        Type::UserConnected       => handle_event(t.clone(), c.clone(), &user_connected, &event),
-                        Type::UserDisconnected    => handle_event(t.clone(), c.clone(), &user_disconnected, &event),
-                        Type::UserStateChanged    => handle_event(t.clone(), c.clone(), &user_state_changed, &event),
-                        Type::UserTextMessage     => handle_event(t.clone(), c.clone(), &user_text_message, &event),
-                        Type::ChannelCreated      => handle_event(t.clone(), c.clone(), &channel_created, &event),
-                        Type::ChannelRemoved      => handle_event(t.clone(), c.clone(), &channel_removed, &event),
-                        Type::ChannelStateChanged => handle_event(t.clone(), c.clone(), &channel_state_changed, &event),
-                    }.await;
+                loop {
+                    if let Ok(Some(event)) = event_stream.message().await {
+                        // the generated method name 'type' conflics with a rust keyword so 'r#' is needed
+                        match event.r#type() {
+                            Type::UserConnected       => handle_event(t.clone(), c.clone(), &user_connected, &event),
+                            Type::UserDisconnected    => handle_event(t.clone(), c.clone(), &user_disconnected, &event),
+                            Type::UserStateChanged    => handle_event(t.clone(), c.clone(), &user_state_changed, &event),
+                            Type::UserTextMessage     => handle_event(t.clone(), c.clone(), &user_text_message, &event),
+                            Type::ChannelCreated      => handle_event(t.clone(), c.clone(), &channel_created, &event),
+                            Type::ChannelRemoved      => handle_event(t.clone(), c.clone(), &channel_removed, &event),
+                            Type::ChannelStateChanged => handle_event(t.clone(), c.clone(), &channel_state_changed, &event),
+                        }.await;
+                    }
                 }
             }
-            true
         })
     };
+
     // CHAT FILTER
     let chat_filter_fut = {
         let mut c = c.clone();
@@ -396,44 +398,42 @@ where T: Send + Clone + 'static,
                 let mut filter_stream = c.text_message_filter(r).await
                     .expect("Connecting to filter stream")
                     .into_inner();
-                while let Ok(Some(mut filter)) = filter_stream.message().await {
-                    /*
-                    for chat_filter in chat_filters.iter() {
-                        if !(chat_filter)(t.clone(), c.clone(), &mut filter).await {
-                            break;
+                loop {
+                    if let Ok(Some(mut filter)) = filter_stream.message().await {
+                        for chat_filter in chat_filters.iter() {
+                            if !(chat_filter)(t.clone(), c.clone(), &mut filter).await { break; }
                         }
+                        s.send(filter.clone()).await
+                            .expect("Sending filter to stream");
                     }
-                    */
-                    s.send(filter.clone()).await
-                        .expect("Sending filter to stream");
                 }
-                println!("exit");
             }
         })
     };
+
     // AUTHENTICATOR
     let authenticator_fut = {
         let mut c = c.clone();
         let t = t.clone();
         let (mut s, r) = mpsc::channel(CHANNEL_BUFFER_SIZE);
-        future_from_async(async move {
+        Box::pin(async move {
             if !authenticators.is_empty() {
                 let mut authenticator_stream = c.authenticator_stream(r).await
                     .expect("Connecting to authenticator stream")
                     .into_inner();
-                while let Ok(Some(request)) = authenticator_stream.message().await {
-                    let mut response = Response::default();
-                    for authenticator in authenticators.iter() {
-                        if !(authenticator)(t.clone(), c.clone(), &mut response, &request).await {
-                            break;
+                loop {
+                    if let Ok(Some(request)) = authenticator_stream.message().await {
+                        let mut response = Response::default();
+                        for authenticator in authenticators.iter() {
+                            if !(authenticator)(t.clone(), c.clone(), &mut response, &request).await { break; }
                         }
+                        s.send(response).await.unwrap();
                     }
-                    s.send(response).await.unwrap();
                 }
             }
-            true
         })
     };
+
     // CONTEXT MENU ACTIONS
     let context_action_fut = {
         let c = c.clone();
@@ -441,24 +441,26 @@ where T: Send + Clone + 'static,
         join_all(context_actions.into_iter().map(|(action, handlers)| {
             let mut c = c.clone();
             let t = t.clone();
-            future_from_async(async move {
+            Box::pin(async move {
                 if !handlers.is_empty() {
                     let mut context_action_stream = c.context_action_events(action).await
                         .expect("Connecting to context action stream")
                         .into_inner();
-                    while let Ok(Some(context_action)) = context_action_stream.message().await {
-                        for handler in handlers.iter() {
-                            if !(handler)(t.clone(), c.clone(), &context_action).await {
-                                break;
+                    loop {
+                        if let Ok(Some(context_action)) = context_action_stream.message().await {
+                            for handler in handlers.iter() {
+                                if !(handler)(t.clone(), c.clone(), &context_action).await {
+                                    break;
+                                }
                             }
                         }
                     }
                 }
-                true
             })
         }))
     };
 
+    // join all the tasks into a single future
     drop(join!(server_event_fut, authenticator_fut, chat_filter_fut, context_action_fut));
 
     for disconnect_handler in server_disconnected.iter() {
